043aac2d8ee935ff021f43e35973a301826fa6f1
ArshadulAhmed <ahmed.arshadul@gmail.com>
2021-02-05 13:06:17 +0530

    deployment

--- frontend/src/redux/utils/baseURL.js
+++ frontend/src/redux/utils/baseURL.js
@@ -1,1 +1,3 @@
-export const baseURL = "http://localhost:8080/api";
+// export const baseURL = "http://localhost:8080/api";
+
+export const baseURL = "https://userauthenticationsystem.herokuapp.com/api";
